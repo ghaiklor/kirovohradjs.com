b8efe27226fc958f112b68f5fd51532c624b1181
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-03 17:31:01 +0300

    Add smooth scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 (function () {
   $(document).foundation();
+
+  smoothScroll.init({
+    speed: 1000,
+    offset: 50
+  });
+
   $('section.place .row.embed').click(function () {
     $(this).find('iframe').css('pointer-events', 'auto');
   });
